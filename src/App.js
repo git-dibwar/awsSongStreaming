e1045c17907224b117395233e55eac10a51d8e09
Dibwarjeet Bordolai <dibwarjeetb@gmail.com>
2022-12-25 13:59:16 +0530

    rectified the titles of the video

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ function App() {
   return (
     <div className="App">
       <h1>Video Streaming site</h1>
-      <h2>Rivers And Roads</h2>
+      <h2>Coin Operated - Animated Short Film</h2>
       <video with="700px" height="400px" controls>
         <source src="https://d2rs871f9yu3lb.cloudfront.net/Coin Operated - Animated Short Film.mp4" type="video/mp4" />
       </video>
       <div>
         <h4>---------------------------------------</h4>
       </div>
-      <h2>Coin Operated - Animated Short Film</h2>
+      <h2>Current Joys A different age</h2>
       <video with="700px" height="400px" controls>
         <source src="https://d2rs871f9yu3lb.cloudfront.net/Current_Joys_A_different_age.mp4" type="video/mp4" />
       </video>
