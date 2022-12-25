7bae69bae89325dd5da40cfbaea23d40cf51172b
Dibwarjeet Bordolai <dibwarjeetb@gmail.com>
2022-12-25 13:46:59 +0530

    added new video

--- src/App.js
+++ src/App.js
@@ -7,9 +7,12 @@ function App() {
       <h1>Music Streaming site</h1>
       <h2>Rivers And Roads</h2>
       <video with="700px" height="400px" controls>
-        <source src="https://d2rs871f9yu3lb.cloudfront.net/RiversAndRoads.mp4" type="video/mp4" />
+        <source src="https://d2rs871f9yu3lb.cloudfront.net/Coin Operated - Animated Short Film.mp4" type="video/mp4" />
       </video>
-      <h2>Current Joys A different age</h2>
+      <div>
+        <h4>---------------------------------------</h4>
+      </div>
+      <h2>Coin Operated - Animated Short Film</h2>
       <video with="700px" height="400px" controls>
         <source src="https://d2rs871f9yu3lb.cloudfront.net/Current_Joys_A_different_age.mp4" type="video/mp4" />
       </video>
