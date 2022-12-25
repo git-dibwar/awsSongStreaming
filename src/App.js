4bd48604305fc033a1590ea463e2948a474e7679
Dibwarjeet Bordolai <dibwarjeetb@gmail.com>
2022-12-25 13:17:11 +0530

    added title

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Songs</h1>
+      <h1>Music Streaming site</h1>
       <h2>Rivers And Roads</h2>
       <video with="700px" height="400px" controls>
         <source src="https://d2rs871f9yu3lb.cloudfront.net/RiversAndRoads.mp4" type="video/mp4" />
